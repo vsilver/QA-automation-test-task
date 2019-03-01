f9d7646a6c7db2456e8b4202727b3fc414059583
vsilver <valeriysilvanovich@gmail.com>
2019-03-01 18:39:10 +0200

    initial commit

---
+++ src/test/java/TestAPI.java
@@ -0,0 +1,27 @@
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.RestAssured.basic;
+import static org.hamcrest.Matchers.*;
+
+
+public class TestAPI {
+    protected static void setupRestAssured() {
+        RestAssured.baseURI = "graph.facebook.com";
+    }
+
+    @BeforeTest
+    public void setUp() {
+        setupRestAssured();
+    }
+
+    
+
+}
+
